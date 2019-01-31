e90465f47ffb421a64e89ec0df434386b0194d57
Jonah Tuckman <JonahTuckman@Jonahs-MacBook-Pro.local>
2019-01-31 17:43:09 -0500

    Slight changes to layout

--- Lab0/Lab0.R
+++ Lab0/Lab0.R
@@ -1,22 +1,20 @@
-install.packages("knitr")
-library(knitr)
-install.packages(('rmarkdown'))
-
- <!-- Instructions: This is the template you will use to type up your responses to the exercises and the on your own questions. To produce a document that you can print out and turn in just click on Knit HTML above. All you need to do to complete the lab is to type up your BRIEF answers and the R code (when necessary) in the spaces provided below. If you want to find out more about the markdown language click on the MD icon. -->
+---
+  output:
+  html_document: default
+word_document: default
+pdf_document: default
+---
+  <!-- Instructions: This is the template you will use to type up your responses to the exercises and the on your own questions. To produce a document that you can print out and turn in just click on Knit HTML above. All you need to do to complete the lab is to type up your BRIEF answers and the R code (when necessary) in the spaces provided below. If you want to find out more about the markdown language click on the MD icon. -->
   
-  ---
-  ### title: Lab0
+  Lab 0
+========================================================
   
-  ### author: 'Jonah Tuckman'
-  --- 
-  ### Discussants (other team members present in lab):[Enter discussant names here]
-  
+  ### Author: Jonah Tuckman
   
   #### Load Arbuthnot's data:
-
+  ```{r}
 source("http://www.openintro.org/stat/data/arbuthnot.R")
-
-
+```
 
 #### Exercise 1: 
 
@@ -24,41 +22,44 @@ source("http://www.openintro.org/stat/data/arbuthnot.R")
 ```{r}
 # code for Exercise 1 is already entered below as an example
 arbuthnot$girls
-#plot(arbuthnot$girls)
+# plot(arbuthnot$girls)
 plot(x = arbuthnot$year, y = arbuthnot$girls, type = "b")
 ```
 
 #### Exercise 2:
-## Is there an apparent trend in the # girls baptized?
-# Yes, they gradually fell in the mid 1600s then grew rapidly after 1660.
 
+## Is there an apparent trend in the # girls baptized?
+#### Yes, they gradually fell in the mid 1600s then grew rapidly after 1660.
+
+#### Personal Code bellow  
 totalBap = arbuthnot$boys + arbuthnot$girls
 plot(arbuthnot$year, totalBap, type = 'b')
 
 Proportion =  arbuthnot$boys / arbuthnot$girls
 newBornBoys = arbuthnot$boys/(totalBap)
 
+
 #### Exercise 3:
+
+
 ```{r fig.width=7, fig.height=5}
+totalBap = arbuthnot$boys + arbuthnot$girls
+newBornBoys = arbuthnot$boys/(totalBap)
 plot(arbuthnot$year, newBornBoys, type = 'b')
 # This is a very volitile graph showing the proportion of boys over time
 # The boys bounce around the line of about 51% meaning they generally stay at that portion 
 # of the population 
 
 ```
-
-
-
 arbuthnot$boys > arbuthnot$girls # True every single year
-
 
 #### Load present day data:
 ```{r}
 source("http://www.openintro.org/stat/data/present.R")
+
 ```
 
 #### Exercise 4:
-
 present
 dim(present) 
 # 1940 - 2002
@@ -80,7 +81,6 @@ max(arbuthnot)
 #### Exercise 6:
 
 
-
 ```{r fig.width=7, fig.height=5}
 boyToGirl = present$boys / present$girls
 plot(present$year, boyToGirl, type ='b')
@@ -89,14 +89,11 @@ plot(present$year, boyToGirl, type ='b')
 
 ```
 
-
-
 #### Exercise 7:
 
-```{r present}
+```{r}
 max(present)
 present
 # This max of 2186274 was achieved in 1961 by boys
 ```
-
 
