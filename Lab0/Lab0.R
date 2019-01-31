df12298f4ee788dc9f80953d62818b797a86b2b7
Jonah Tuckman <JonahTuckman@Jonahs-MacBook-Pro.local>
2019-01-31 17:37:11 -0500

    Finished R program for Lab0, converted into html markdown file using knit

--- Lab0/Lab0.R
+++ Lab0/Lab0.R
@@ -1,10 +1,14 @@
-#<!-- Instructions: This is the template you will use to type up your responses to the exercises and the on your own questions. To produce a document that you can print out and turn in just click on Knit HTML above. All you need to do to complete the lab is to type up your BRIEF answers and the R code (when necessary) in the spaces provided below. If you want to find out more about the markdown language click on the MD icon. -->
+install.packages("knitr")
+library(knitr)
+install.packages(('rmarkdown'))
+
+ <!-- Instructions: This is the template you will use to type up your responses to the exercises and the on your own questions. To produce a document that you can print out and turn in just click on Knit HTML above. All you need to do to complete the lab is to type up your BRIEF answers and the R code (when necessary) in the spaces provided below. If you want to find out more about the markdown language click on the MD icon. -->
   
-#  Lab 0
-#========================================================
+  ---
+  ### title: Lab0
   
-  ### Author: Jonah Tuckman
-  
+  ### author: 'Jonah Tuckman'
+  --- 
   ### Discussants (other team members present in lab):[Enter discussant names here]
   
   
@@ -12,18 +16,17 @@
 
 source("http://www.openintro.org/stat/data/arbuthnot.R")
 
-install.packages("knitr")
-library(knitr)
+
 
 #### Exercise 1: 
 
 
-#```{r}
+```{r}
 # code for Exercise 1 is already entered below as an example
 arbuthnot$girls
 #plot(arbuthnot$girls)
 plot(x = arbuthnot$year, y = arbuthnot$girls, type = "b")
-#```
+```
 
 #### Exercise 2:
 ## Is there an apparent trend in the # girls baptized?
@@ -36,23 +39,23 @@ Proportion =  arbuthnot$boys / arbuthnot$girls
 newBornBoys = arbuthnot$boys/(totalBap)
 
 #### Exercise 3:
-#```{r fig.width=7, fig.height=5}
-# enter R code for exercise 3 below,
-
-#```
-
+```{r fig.width=7, fig.height=5}
 plot(arbuthnot$year, newBornBoys, type = 'b')
 # This is a very volitile graph showing the proportion of boys over time
 # The boys bounce around the line of about 51% meaning they generally stay at that portion 
 # of the population 
 
+```
+
+
+
 arbuthnot$boys > arbuthnot$girls # True every single year
 
 
 #### Load present day data:
-#```{r}
+```{r}
 source("http://www.openintro.org/stat/data/present.R")
-#```
+```
 
 #### Exercise 4:
 
@@ -73,27 +76,27 @@ present
 # the arb dataset was 8426.
 max(present)
 max(arbuthnot)
+
 #### Exercise 6:
 
+
+
+```{r fig.width=7, fig.height=5}
 boyToGirl = present$boys / present$girls
 plot(present$year, boyToGirl, type ='b')
 # The ratio of boys to girls being baptized is falling to be closer to 1 to 1 over time.
 # Boys are still in greater proportion than girls but this trend is now downard sloping and approaching equality.
 
+```
 
-#```{r fig.width=7, fig.height=5}
-# enter R code for exercise 6 below
 
-#```
 
 #### Exercise 7:
 
-#```{r}
-# enter R code for exercise 7 below
-
-#```
+```{r present}
 max(present)
 present
 # This max of 2186274 was achieved in 1961 by boys
+```
 
 
