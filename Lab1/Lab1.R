741a45a76f00bef2effe15dfac97bb21b9ee0267
JonahTuckman <jtuckman9@gmail.com>
2019-02-14 11:33:26 -0500

    finished excercised 1-5

---
+++ Lab1/Lab1.R
@@ -0,0 +1,96 @@
+<!-- Instructions: This is the template you will use to type up your responses to the exercises and the on your own questions. To produce a document that you can print out and turn in just click on Knit HTML above. All you need to do to complete the lab is to type up your BRIEF answers and the R code (when necessary) in the spaces provided below. If you want to find out more about the markdown language click on the MD icon. -->
+  
+  Lab 1 
+========================================================
+  
+  #### Team name: Team 4
+  
+  #### Author: Jonah, Miranda, Manavi, Charlotte, Lucy
+  
+  #### Discussants (other team members present in lab): 
+  
+  
+  
+  
+  ### Exercises
+
+#### Load CDC data:
+
+```{r}
+source("http://www.openintro.org/stat/data/cdc.R")
+names(cdc)
+```
+
+#### Exercise 1: 
+head(cdc)
+tail(cdc)
+## There are 20000 responses in the dataset. There are 9 variables in the dataset.
+# genhlth -> ordinal categorical
+# exerany -> unordered (regular) categirical
+# hlthplan -> regular categorical
+# smoke100 -> regular categorical 
+# height -> continuous 
+# weight -> continuous 
+# wtdesire -> continuous 
+# age -> continuous
+# gender -> regular categorical 
+
+
+summary(cdc$weight)
+table(cdc$smoke100)/20000  # percentage
+barplot(table(cdc$smoke100))
+
+
+#### Exercise 2:
+
+
+```{r}
+summary(cdc$height) # 1st -> 64.00 3rd -> 70.00
+summary(cdc$age) # 1st -> 31.00 3rd -> 57.00
+
+IQRHeight <- (70 - 64)
+IQRAge <- (57.00 - 31.00)
+
+
+table(cdc$gender)
+#9569 males in the sample
+
+table(cdc$genhlth)/20000
+# 23% are in excelent health
+
+```
+
+#### Exercise 3:
+
+
+```{r}
+mosaicplot(table(cdc$gender, cdc$smoke100))
+# This revealed that slightly more males smoke than females. 
+
+```
+
+
+#### Exercise 4:
+
+```{r fig.width=7, fig.height=5}
+under23_and_smoke <- subset(cdc, cdc$age < 23 & cdc$smoke100 == 1)
+```
+
+#### Exercise 5:
+
+bmi <- (cdc$weight/cdc$height^2) * 703
+boxplot(bmi ~ cdc$genhlth)
+
+```{r}
+
+```
+
+#### Exercise 6:
+
+```{r fig.width=7, fig.height=5}
+
+```
+
+```{r fig.width=7, fig.height=5}
+
+```
