a3c460d972fceaf0fb126f8ac05f2a9719e08328
JonahTuckman <jtuckman9@gmail.com>
2019-02-23 17:56:54 -0500

    Plot C

--- ProblemSet3/ProblemSet3.R
+++ ProblemSet3/ProblemSet3.R
@@ -8,7 +8,7 @@ qqline(bdims$hgt)
 # (a) The histogram for female biiliac (pelvic) diameter (bii.di) belongs to normal probability plot
 # letter B.
 # (b) The histogram for female elbow diameter (elb.di) belongs to normal probability plot letter C.
-# (c) The histogram for general age (age) belongs to normal probability plot letter .
+# (c) The histogram for general age (age) belongs to normal probability plot letter D.
 # (d) The histogram for female chest depth (che.de) belongs to normal probability plot letter .
 
 
@@ -28,4 +28,10 @@ hist((bdims$elb.di - meanB) / sdB)
 qqnorm(bdims$elb.di)
 qqline(bdims$elb.di) ## Belongs to probability plot C
 
-# 
+# Age
+meanC = mean(bdims$age)
+sdC = sd(bdims$age)
+hist((bdims$age - meanC) / sdC)
+
+qqnorm(bdims$age)
+qqline(bdims$age) ## Belongs to probability plot D
