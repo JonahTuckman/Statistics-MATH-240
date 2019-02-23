043973d6219e7246e56d3b004eabb92c20822dd0
JonahTuckman <jtuckman9@gmail.com>
2019-02-23 17:54:08 -0500

    added Problem set 3 directory, finished plots A and B

---
+++ ProblemSet3/ProblemSet3.R
@@ -0,0 +1,31 @@
+download.file("http://www.openintro.org/stat/data/bdims.RData", destfile = "bdims.RData")
+load("bdims.RData") ## bdims
+
+qqnorm(bdims$hgt)
+qqline(bdims$hgt)
+
+
+# (a) The histogram for female biiliac (pelvic) diameter (bii.di) belongs to normal probability plot
+# letter B.
+# (b) The histogram for female elbow diameter (elb.di) belongs to normal probability plot letter C.
+# (c) The histogram for general age (age) belongs to normal probability plot letter .
+# (d) The histogram for female chest depth (che.de) belongs to normal probability plot letter .
+
+
+# Bii.di
+meanA = mean(bdims$bii.di)
+sdA = sd(bdims$bii.di, na.rm = FALSE)
+hist((bdims$bii.di - meanA) / sdA)
+
+qqnorm(bdims$bii.di)
+qqline(bdims$bii.di) ## Belongs to probability plot B
+
+# Elb.di
+meanB = mean(bdims$elb.di)
+sdB = sd(bdims$elb.di)
+hist((bdims$elb.di - meanB) / sdB)
+
+qqnorm(bdims$elb.di)
+qqline(bdims$elb.di) ## Belongs to probability plot C
+
+# 
