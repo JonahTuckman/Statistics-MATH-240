7c67f1f078796b3b4b71ddbb4a33206e8dc9df00
JonahTuckman <jtuckman9@gmail.com>
2019-03-07 10:43:16 -0500

    ESxcercise 1

--- Lab2/Lab2.R
+++ Lab2/Lab2.R
@@ -4,3 +4,20 @@
 setwd('/Users/JonahTuckman/Desktop/Statistics/Statistics-MATH-240/Lab2')
 download.file("http://www.openintro.org/stat/data/ames.RData", destfile = "ames.RData")
 load("ames.RData")
+
+
+
+#### Excercise 1 -> Describe the population .
+area <- ames$Gr.Liv.Area
+price <- ames$SalePrice
+
+summary(area)
+summary(price)
+
+
+hist(area)
+
+samp0 <- sample(area,50)
+summary(samp0)
+
+
