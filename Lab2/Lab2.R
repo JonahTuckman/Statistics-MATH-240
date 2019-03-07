c406206c997e644c6069555835291a7d7137deb4
JonahTuckman <jtuckman9@gmail.com>
2019-03-07 10:40:04 -0500

    Lab2 Directory and script

---
+++ Lab2/Lab2.R
@@ -0,0 +1,6 @@
+
+
+
+setwd('/Users/JonahTuckman/Desktop/Statistics/Statistics-MATH-240/Lab2')
+download.file("http://www.openintro.org/stat/data/ames.RData", destfile = "ames.RData")
+load("ames.RData")
