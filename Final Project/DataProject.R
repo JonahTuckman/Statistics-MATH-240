94346514a3f18615e5c12468d03769754102f5ea
JonahTuckman <jtuckman9@gmail.com>
2019-04-01 17:54:46 -0400

    Found percentage of rep and dem states which have positive solar output

--- Final Project/DataProject.R
+++ Final Project/DataProject.R
@@ -4,6 +4,7 @@
 ### ConnocoPhillips: Texas
 ### Occidental Petroleum Company: Texas
 ### Valero Industries: Texas
+### Phillips 66: Texas
 
 library("ggpubr")
 library("ggplot2")
@@ -13,7 +14,30 @@ library(tidyverse)
 
 setwd('/Users/JonahTuckman/Desktop/Statistics/Statistics-MATH-240/Final Project')
 
+
 statsData <- read.csv('State RE Generation-Table 1.csv') 
 neededCols <- c("State","Solar")
 statsData <- statsData[neededCols]
 write.csv(statsData, "SolarData.csv")
+
+
+statsData <- read.csv("SolarData.csv")
+statsData['X'] <- NULL ## Deleted unneeded column
+
+replaceCommas<-function(X){
+  X<-as.numeric(gsub("\\,", "", X))
+}
+
+statsData$Solar <- replaceCommas(statsData$Solar)
+
+RepData <- subset(statsData, statsData$Voting == 'R')
+DemData <- subset(statsData, statsData$Voting == 'D')
+
+demSolarNum <- subset(DemData, DemData$Solar>0)
+demSolarPerc <- (nrow(demSolarNum) / nrow(DemData) * 100)
+print(demSolarPerc)
+## 28.57% of Democratic States have a positive solar output
+
+RepSolarNum <- subset(RepData, RepData$Solar > 0)
+RepSolarPerc <-(nrow(RepSolarNum) / nrow(RepData) * 100)
+print(RepSolarPerc)
