3f03afb1639c7b2a92e1fc2205345bd88ac7bb44
JonahTuckman <jtuckman9@gmail.com>
2019-04-01 17:30:58 -0400

    Fixed dataset to show states and solar output

--- Final Project/DataProject.R
+++ Final Project/DataProject.R
@@ -1,1 +1,15 @@
 # Data Source: https://catalog.data.gov/dataset/energy-generation-by-state-and-technology-2009/resource/bb0da868-f498-4c47-b61f-da217897198f
+### Exxon: Texas 
+
+library("ggpubr")
+library("ggplot2")
+library("dplyr")
+library(tidyverse)
+
+
+setwd('/Users/JonahTuckman/Desktop/Statistics/Statistics-MATH-240/Final Project')
+
+statsData <- read.csv('State RE Generation-Table 1.csv') 
+neededCols <- c("State",Solar")
+statsData <- statsData[neededCols]
+write.csv(statsData, "SolarData.csv")
