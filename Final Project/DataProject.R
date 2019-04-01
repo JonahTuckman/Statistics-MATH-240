ed6616713a5249df9a57a71e25ed12dc9a522e4e
JonahTuckman <jtuckman9@gmail.com>
2019-04-01 18:11:57 -0400

    added a few comments

--- Final Project/DataProject.R
+++ Final Project/DataProject.R
@@ -10,13 +10,13 @@ library(tidyverse)
 setwd('/Users/JonahTuckman/Desktop/Statistics/Statistics-MATH-240/Final Project')
 
 
-statsData <- read.csv('State RE Generation-Table 1.csv') 
-neededCols <- c("State","Solar")
-statsData <- statsData[neededCols]
-write.csv(statsData, "SolarData.csv")
+statsData <- read.csv('State RE Generation-Table 1.csv') ## Original dataset with many extra energy types
+neededCols <- c("State","Solar") ## Focusing on states and Solar output
+statsData <- statsData[neededCols] ## Taking only those columns stated above
+write.csv(statsData, "SolarData.csv") ## Writing this new file
 
 
-statsData <- read.csv("SolarData.csv")
+statsData <- read.csv("SolarData.csv") ## Reading this new file
 statsData['X'] <- NULL ## Deleted unneeded column
 
 replaceCommas<-function(X){
@@ -36,3 +36,4 @@ print(demSolarPerc)
 RepSolarNum <- subset(RepData, RepData$Solar > 0)
 RepSolarPerc <-(nrow(RepSolarNum) / nrow(RepData) * 100)
 print(RepSolarPerc)
+## 10% of Republican States have a positive solar output
