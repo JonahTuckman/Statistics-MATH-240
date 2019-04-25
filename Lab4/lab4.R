59472f4b648008a250008220f6bd8646f8cfdd2b
JonahTuckman <jtuckman9@gmail.com>
2019-04-25 10:55:16 -0400

    Through 4 excercises

--- Lab4/lab4.R
+++ Lab4/lab4.R
@@ -4,31 +4,32 @@
 ========================================================
   
   ### Exercises
-  
+  setwd('/Users/JonahTuckman/Desktop/Statistics/Statistics-MATH-240/Lab4')
+
   #### Load inference function:
   ```{r}
-
+load(url('http://s3.amazonaws.com/assets.datacamp.com/course/dasi/inference.Rdata'))
 ```
 
 #### Load GSS data:
 ```{r}
-
+gss = read.csv("/Users/JonahTuckman/Desktop/Spring2019/Statistics/Statistics-MATH-240/Lab4/gss_wordsum_class.csv")
 ```
+
+
+test = inference(y = gss$wordsum, x = gss$class, est = "mean", method = "theoretical",
+          type = "ht", alternative = "greater")
 
 #### Exercise 1:
 
-
 #### Exercise 2: 
 
+## Group did these parts
 
 #### Load Atheism data:
 ```{r}
-
+athiest = read.csv("/Users/JonahTuckman/Desktop/Spring2019/Statistics/Statistics-MATH-240/Lab4/atheism.csv")
 ```
-
-#### Exercise 3: 
-
-#### Exercise 4: 
 
 #### Exercise 5: 
 
