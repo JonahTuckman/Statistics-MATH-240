ad3a2401b5cd5a63a11834288afa8b3292af4f1c
JonahTuckman <jtuckman9@gmail.com>
2019-04-25 10:33:15 -0400

    Lab4

---
+++ Lab4/lab4.R
@@ -0,0 +1,51 @@
+<!-- Instructions: This is the template you will use to type up your responses to the exercises and the on your own questions. To produce a document that you can print out and turn in just click on Knit HTML above. All you need to do to complete the lab is to type up your BRIEF answers and the R code (when necessary) in the spaces provided below. If you want to find out more about the markdown language click on the MD icon. -->
+  
+  Lab 4
+========================================================
+  
+  ### Exercises
+  
+  #### Load inference function:
+  ```{r}
+
+```
+
+#### Load GSS data:
+```{r}
+
+```
+
+#### Exercise 1:
+
+
+#### Exercise 2: 
+
+
+#### Load Atheism data:
+```{r}
+
+```
+
+#### Exercise 3: 
+
+#### Exercise 4: 
+
+#### Exercise 5: 
+
+#### Exercise 6: 
+
+#### Exercise 7:
+
+#### Exercise 8:
+
+#### Exercise 9: 
+
+#### Exercise 10: 
+
+#### Exercise 11a: 
+
+#### Exercise 11b: 
+
+#### Exercise 12:
+
+#### Exercise 13:
